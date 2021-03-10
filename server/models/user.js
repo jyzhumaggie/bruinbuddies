5ab3f0c84c182760632b60525672ea5b5b33dee1
Maggie Zhu <jyzhumaggie@gmail.com>
2021-03-10 11:00:07 -0600

    adding to user model

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,16 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true},
     password: { type: String, required: true},
-    id: { type: String }
+    id: { type: String },
+    
+    bio: { type: String, required: true },
+
+    major: { type: String, required: true },
+    year: { type: String, required: true },
+    hobbies: { type: String, required: true },
+    catDog: { type: String, required: true },
+    nightOrMorning: { type: String, required: true },
+    groupSize: { type: String, required: true },
 });
 
 export default mongoose.model('User', userSchema);
