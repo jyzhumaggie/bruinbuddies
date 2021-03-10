952da92cb5d0c5020e66720dc9885328999330cf
Maggie Zhu <jyzhumaggie@gmail.com>
2021-03-10 16:18:06 -0600

    finished searching posts function

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -14,32 +14,39 @@ const handleSearch = () => {
 
 const Search = () => {
     const posts = useSelector((state) => state.posts);  // reducers/index.js: combineReducers posts
+    const users = useSelector((state) => state.users);  // reducers/index.js: combineReducers posts
+    
     const user = JSON.parse(localStorage.getItem('profile'));
-    console.log(user?.result?.name);
-    console.log(posts[0]);
-    console.log(posts[0]?.title);
-    console.log(posts[1]?.title);
-    console.log(posts[4]?.title);
-    console.log(posts[1]);
-    // console.log("query is " + query);
-
 
     const { search } = window.location;
     const query = new URLSearchParams(search).get('s');
     
     const filterPosts = (posts, query) => {
-        console.log("filtering" + query);
         if (!query) {
-            return posts;
+            return null;
         } else {
             return posts.filter((post) => {
-                const postTitle = post?.title.toLowerCase() + post?.message.toLowerCase();
-                return postTitle.includes(query);
+                const postTitle = post?.title?.toLowerCase() + post?.message?.toLowerCase() + post?.name?.toLowerCase();
+                return postTitle.includes(query.toLowerCase());
             })
         }
     }
+    // console.log(users);
+    // const filterUsers = (users, query) => {
+    //     if (!query) {
+    //         return null;
+    //     } else {
+    //         return users.filter((post) => {
+    //             const userTitle = user?.title?.toLowerCase() + user?.message?.toLowerCase() + user?.name?.toLowerCase();
+    //             return userTitle.includes(query.toLowerCase());
+    //         })
+    //     }
+    // }
 
-    const [searchQuery, setSearchQuery] = useState(query | '');
+    console.log(query);    
+    console.log("query is " + query);
+
+    const [searchQuery, setSearchQuery] = useState(query | ' ' );
     const filteredPosts = filterPosts(posts, searchQuery);
     
 
@@ -62,14 +69,20 @@ const Search = () => {
                 <button className="searchButton" type="submit">Search</button>
             </form>
             <div className="back">
-                <p className="subsectionTitle"> Posts </p>
-                <Grid className="searchResultList" container alignItems="stretch" spacing={4}>
-                {filteredPosts.map(post => (
-                        <Grid className="searchPostResult" item alignItems="stretch" spacing={4}>
-                            <Post post={post} className="posts" />
+                {
+                    filteredPosts && (
+                    <>
+                        <p className="subsectionTitle"> Posts </p>
+                        <Grid className="searchResultList" container alignItems="stretch" spacing={4}>
+                        {filteredPosts.map(post => (
+                            <Grid className="searchPostResult" item alignItems="stretch" spacing={4}>
+                                <Post post={post} className="posts" />
                             </Grid>
-                )) }
-                </Grid>
+                        ))}
+                        </Grid>
+                    </>
+                    )
+                }
             </div>
         </div>
     )
