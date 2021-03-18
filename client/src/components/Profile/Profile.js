f3857143f3805c54cfa529d31fb11b5c0114a4e2
Maggie Zhu <jyzhumaggie@gmail.com>
2021-03-18 12:47:10 -0500

    fixed bug in accessing personal information

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,17 +1,29 @@
 
 import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux'; 
+import { useDispatch, useSelector } from 'react-redux';
 import { getUsers } from '../../actions/users';
 import './Profile.css';
 
 
 
 const Profile = () => {
-const dispatch = useDispatch(getUsers());
+	const dispatch = useDispatch(getUsers());
 
-const user = JSON.parse(localStorage.getItem('profile'));
+	const user = JSON.parse(localStorage.getItem('profile'));
+	const allUsers = useSelector((state) => state.posts);  // reducers/index.js: combineReducers posts
+	// console.log(user);
 
-console.log(user);
+	const myInfo = allUsers.filter((oneUser) => {
+		if (oneUser?.email === user?.result?.email) {
+			return oneUser;
+		}
+		return null;
+	})
+	console.log(myInfo[0]);
+	const userHere = myInfo[0];
+	console.log(userHere);
+
+
 // where to dispatch the action, inside useEffect
   useEffect(() => {
 
@@ -20,38 +32,38 @@ console.log(user);
 
 
 
-const userHere = user;
+
 
 // console.log(userHere?.result?.selectedFile);
   return (
     	<div className="profile">
     		<div className="picture">
 		
-				<img className="profilePic" src={userHere?.result?.selectedFile} alt="profile"/>
+				<img className="profilePic" src={userHere?.selectedFile} alt="profile"/>
 			</div>
 			<div className="name">
-				<h1>{userHere?.result?.name}</h1>
+				<h1>{userHere?.name}</h1>
 			</div>
 
       		<div className="body">
     
     
-				<h2 className="valueFields"> {userHere?.result?.bio} </h2>
+				<h2 className="valueFields"> {userHere?.bio} </h2>
 				<h2> Year: </h2>
-					<h2 className="valueFields">{userHere?.result?.year}</h2>
+					<h2 className="valueFields">{userHere?.year}</h2>
 				<h2> Major: </h2>
-				<h2 className="valueFields">{userHere?.result?.major}</h2>
+				<h2 className="valueFields">{userHere?.major}</h2>
 				 
 				<h2>Hobbies:</h2> 
-				<h2 className="valueFields">{userHere?.result?.hobbies}</h2>
+				<h2 className="valueFields">{userHere?.hobbies}</h2>
 
 					
 				<h2>Are you a cat person or a dog person?:</h2>
-				<h2 className="valueFields"> {userHere?.result?.catDog} </h2>
+				<h2 className="valueFields"> {userHere?.catDog} </h2>
 				<h2>Do you prefer to study in the morning or night?:</h2>
-				<h2 className="valueFields">{userHere?.result?.nightOrMorning} </h2>
+				<h2 className="valueFields">{userHere?.nightOrMorning} </h2>
         		<h2>Do you prefer to study in smaller or larger groups?: </h2>
-				<h2 className="valueFields"> {userHere?.result?.groupSize} </h2> 
+				<h2 className="valueFields"> {userHere?.groupSize} </h2> 
         
 	        </div>
     	</div>
